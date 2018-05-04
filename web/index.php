032de13aab573796c92391b386cec49e9565eb11
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:51:22 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -7,6 +7,10 @@
 		
 		<?php include 'navbar.php'; ?>
 
+		<ul id="sidebar">
+			<li id="sidebarItem"><a id="sidebarLink" href="assignmentPage.php">Assignment Page</a></li>
+		</ul>";
+
 		<div style="margin-left:25%; padding: 1px 16px; height:1000px;">
 			<img src="picture.jpg" style="width:480px; height: 320px;">
 			<p id="outline">Hi my name is Jason Pyle</p>
