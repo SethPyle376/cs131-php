78613f74ab30d38cdd0c65629894263b386a614f
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:03:34 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -7,5 +7,17 @@
 		<ul id="navbar">
 			<li id="navbarItem"><a id="navbarLink" href="#home">Jason Pyle's Homepage</a></li>
 		</ul>
+
+		<ul id="sidebar">
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+			<li id="sidebarItem"><a id="sidebarLink" href="week2.php">Coming Soon</a></li>
+		</ul>
+
 	</body>
 </html>
