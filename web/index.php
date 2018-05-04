f8a38998abd3e611a07e9f0f760a6a74c49ec732
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:47:04 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -5,8 +5,8 @@
 	</head>
 	<body bgcolor="#b7b2aa">
 		
-		<?
-			php include 'navbar.php';
+		<?php 
+			include 'navbar.php';
 		?>
 
 		<div style="margin-left:25%; padding: 1px 16px; height:1000px;">
