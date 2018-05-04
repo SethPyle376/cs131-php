d36397b38398dfec47ba4dee5a278c2cd10aa466
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:29:47 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -3,7 +3,7 @@
 		<title>Jason Pyle's Homepage</title>
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
-	<body bgcolor="#dddad4">
+	<body bgcolor="#b7b2aa">
 		<ul id="navbar">
 			<li id="navbarItem"><a id="navbarLink" href="#home">Jason Pyle's Homepage</a></li>
 		</ul>
