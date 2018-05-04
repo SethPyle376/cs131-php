d6cc0191088714b2508f362fdbc3c1e34432dc23
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:21:56 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -14,6 +14,7 @@
 
 		<div style="margin-left:25%; padding: 1px 16px; height:1000px;">
 			<img src="picture.jpg" style="width:480px; height: 320px;">
+			<p>Hi my name is Jason Pyle
 		</div>
 
 	</body>
