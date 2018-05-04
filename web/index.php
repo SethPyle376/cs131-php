3ff2617ddd30908ab68e9dfc1583c7b473b01590
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 10:42:42 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -4,13 +4,10 @@
 		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body bgcolor="#b7b2aa">
-		<ul id="navbar">
-			<li id="navbarItem"><a id="navbarLink" href="#home">Jason Pyle's Homepage</a></li>
-		</ul>
-
-		<ul id="sidebar">
-			<li id="sidebarItem"><a id="sidebarLink" href="assignmentPage.php">Assignment Page</a></li>
-		</ul>
+		
+		<?
+			php include 'navbar.php';
+		?>
 
 		<div style="margin-left:25%; padding: 1px 16px; height:1000px;">
 			<img src="picture.jpg" style="width:480px; height: 320px;">
---
+++ web/navbar.php
@@ -0,0 +1,9 @@
+<?php
+	echo "<ul id=\"navbar\">
+			<li id=\"navbarItem\"><a id=\"navbarLink\" href=\"#home\">Jason Pyle's Homepage</a></li>
+		</ul>
+
+		<ul id=\"sidebar\">
+			<li id=\"sidebarItem\"><a id=\"sidebarLink\" href=\"assignmentPage.php\">Assignment Page</a></li>
+		</ul>";
+?>
