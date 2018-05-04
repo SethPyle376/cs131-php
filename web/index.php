f9e07fedf54e8369ab702cdd1764d87aa55da230
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 09:42:55 -0600

    Change

--- web/index.php
+++ web/index.php
@@ -1,5 +1,11 @@
-<?php
 
-  phpinfo();
-
-?>
+<!DOCTYPE html>
+	<head>
+		<title>Jason Pyle's Homepage</title>
+	</head>
+	<body>
+		<ul>
+			<li>Jason Pyle's Homepage</li>
+		</ul>
+	</body>
+</html>
