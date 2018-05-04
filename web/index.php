59add523ad5b6de817f1da7a509e5409540aa320
Jason Pyle <seth.pyle86@gmail.com>
2018-05-04 09:47:15 -0600

    commit

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
-
 <!DOCTYPE html>
 	<head>
 		<title>Jason Pyle's Homepage</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
 	</head>
 	<body>
 		<ul>
-			<li>Jason Pyle's Homepage</li>
+			<li><a class="active" href="#home">Jason Pyle's Homepage</a></li>
 		</ul>
 	</body>
 </html>
