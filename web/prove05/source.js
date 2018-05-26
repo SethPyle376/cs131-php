39120181267589366aca93b58ea883f713a6050c
Jason Pyle <seth.pyle86@gmail.com>
2018-05-26 13:02:22 -0600

    started prove05

--- web/prove05/source.js
+++ web/prove05/source.js
@@ -2,6 +2,7 @@ function getMessage(id) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("POST", "getMessage.php", true);
 	xhttp.onreadystagechange = function() {
+		alert(this.responseText);
 		document.getElementById("messageContainer").innerHTML = this.responseText + document.getElementById("messageContainer").innerHTML;
 	}
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
