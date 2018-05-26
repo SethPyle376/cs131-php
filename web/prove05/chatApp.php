2ab8c779596936eab1a6825cc42c6d6923e62cd6
Jason Pyle <seth.pyle86@gmail.com>
2018-05-26 11:43:20 -0600

    started prove05

---
+++ web/prove05/chatApp.php
@@ -0,0 +1,27 @@
+<html>
+
+	<head>
+		<title>Chat App</title>
+		<link rel="stylesheet" href="style.css">
+		<script src="source.js"></script>
+	</head>
+
+	<body>
+
+		<input type="text" id="messageID" value="" placeholder="Enter Message ID">
+		<button type="button" name="request" onClick="getMessage(document.getElementById('messageID').value" value="Get Message">
+
+		<div id="messageContainer">
+			
+		<div class = "card">
+			<div class="container">
+				<b>User: Seth</b>
+				<p>This is the message</p>
+			</div>
+		</div>
+
+		</div>
+
+	</body>
+
+</html>
---
+++ web/prove05/source.js
@@ -0,0 +1,11 @@
+function getMessage(id) {
+	var xhttp = new XMLHttpRequest();
+	xhttp.open("POST", "getMessage.php", true);
+	xhttp.onreadystagechange = function() {
+		if (this.status = 200) {
+			document.getElementById("messageSpace").innerHtml = (document.getElementById("messageSpace").innerHtml + this.responseText);
+		}
+	}
+	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhttp.send("id=" + id);
+}
