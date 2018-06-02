fc99d30e1db0d6bacf0ca63a5786457ee3593e74
Jason Pyle <seth.pyle86@gmail.com>
2018-06-02 11:21:36 -0600

    fixed php target

--- web/prove06/source.js
+++ web/prove06/source.js
@@ -50,7 +50,7 @@ function userRedirect() {
 
 function getNickname(cookie) {
 	var xhttp = new XMLHttpRequest();
-	xhttp.open("POST", "getMessage.php", true);
+	xhttp.open("POST", "getNickname.php", true);
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
 			document.getElementById("top").innerHTML = this.responseText + document.getElementById("top").innerHTML;
