3c6c6ccd1845a27cf98c3697fa9ba6bcf7280bc2
Jason Pyle <seth.pyle86@gmail.com>
2018-06-02 12:11:25 -0600

    fixed php target

--- web/prove06/source.js
+++ web/prove06/source.js
@@ -75,7 +75,7 @@ function retrieveMessage() {
 	xhttp.open("POST", "getMessage.php", true);
 	xhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status == 200) {
-			document.getElementById("messageContent").innerHTML = this.responseText+ document.getElementById("messageContent").innerHTML;
+			document.getElementById("messageContainer").innerHTML = this.responseText + document.getElementById("messageContainer").innerHTML;
 		}
 	}
 	xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
